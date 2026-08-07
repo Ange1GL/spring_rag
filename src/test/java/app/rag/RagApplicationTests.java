54b4dfc8806062de721119df5e9ccdecdb0cef4c
Angel Larios <alarios@casystem.com.mx>
2026-08-06 19:43:45 -0600

    setup inicial de spring con ai

---
+++ src/test/java/app/rag/RagApplicationTests.java
@@ -0,0 +1,13 @@
+package app.rag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
